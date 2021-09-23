bf012409efbacb2a391ff554f4c194a5b4bd107f
Aman Singh <ars.arsingh@gmail.com>
2021-09-23 11:07:30 +0530

    fix : errorHandler function

--- area.js
+++ area.js
@@ -4,6 +4,13 @@ const thirdSide = document.querySelector("#third-side");
 const areaMessage = document.querySelector("#area-message");
 const areaForm = document.querySelector("#area-form")
 
+function errorHandler(sideOne, sideTwo, sideThree) {
+    if((sideOne > (sideTwo + sideThree)) || (sideTwo > (sideOne + sideThree)) || (sideThree > (sideOne + sideTwo))) {
+        areaMessage.innerText = "Each side length should be less than the sum of other two sides!!"
+        areaMessage.style.color = "red";
+    }
+}
+
 function calculateSemiPerimeter (a, b, c) {
     const semiPerimeter = ((a + b + c) / 2); 
     return semiPerimeter;
@@ -13,7 +20,9 @@ function calculateAreaOfTriangle (event) {
     event.preventDefault();
     const s = calculateSemiPerimeter(Number(firstSide.value), Number(secondSide.value), Number(thirdSide.value));
     const areaOfTriangle = Math.sqrt(s * (s - Number(firstSide.value)) * (s - Number(secondSide.value)) * (s - Number(thirdSide.value)));
+    areaMessage.style.color = "black";
     areaMessage.innerText  = "Area of a triangle using heron's formula is " + areaOfTriangle.toFixed(4) + " units."
+    errorHandler(Number(firstSide.value), Number(secondSide.value), Number(thirdSide.value));
 }
 
 areaForm.addEventListener("submit", calculateAreaOfTriangle);
